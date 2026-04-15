31917312b9946a8ce19557a66f7e930cbaf20fc9
Dani3453 <danielrteixeira8@gmail.com>
2026-04-15 09:59:43 +0100

    equipamentos feito
    finalização equipamentos

--- Assets/sripts/CarrosselSimples.cs
+++ Assets/sripts/CarrosselSimples.cs
@@ -1,35 +1,58 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using UnityEngine.SceneManagement; 
 
 public class CarrosselSimples : MonoBehaviour
 {
+    [Header("Componentes de UI")]
     public Image displayImagem;
-    public TextMeshProUGUI textoTitulo;
-    public Sprite[] imagens;
+    public TextMeshProUGUI campoTitulo;
+    public TextMeshProUGUI campoResumo;
 
-    
-    public static int itemSelecionado;
+    [Header("As tuas 5 Imagens")]
+    public Sprite[] listaImagens;
 
-    private string[] titulos = { "Fato Espacial (EMU)", "Telescópio James Webb", "Braço Canadarm2", "Painéis Solares", "Escudo Térmico" };
+    private string[] titulos = {
+        "Fato Espacial (EMU)",
+        "Telescópio James Webb",
+        "Braço Canadarm2",
+        "Painéis Solares",
+        "Escudo Térmico"
+    };
+
+    private string[] resumos = {
+        "É uma 'nave minúscula'. Protege contra o vácuo, temperaturas de -150°C a 120°C e micrometeoritos.",
+        "Sucessor do Hubble. Observa em infravermelho, permitindo ver através de nuvens de poeira cósmica.",
+        "Braço de 17 metros na ISS. Move-se como um verme e ajuda a capturar naves de carga.",
+        "Fonte de energia para missões. Os da ISS cobririam quase oito campos de basquetebol.",
+        "Essencial para o regresso. Suporta a fricção que gera temperaturas de quase 1600°C."
+    };
+
     private int indice = 0;
 
-    void Start() { AtualizarEcra(); }
-
-    public void Proximo() { indice = (indice + 1) % titulos.Length; AtualizarEcra(); }
-    public void Anterior() { indice--; if (indice < 0) indice = titulos.Length - 1; AtualizarEcra(); }
-
-    void AtualizarEcra()
+    void Start()
     {
-        displayImagem.sprite = imagens[indice];
-        textoTitulo.text = titulos[indice];
+        AtualizarInterface();
     }
 
- 
-    public void IrParaDetalhe()
+    public void Proximo()
     {
-        itemSelecionado = indice; 
-        SceneManager.LoadScene("Cena_Detalhe_Equipamento"); 
+        indice = (indice + 1) % titulos.Length;
+        AtualizarInterface();
+    }
+
+    public void Anterior()
+    {
+        indice--;
+        if (indice < 0) indice = titulos.Length - 1;
+        AtualizarInterface();
+    }
+
+    void AtualizarInterface()
+    {
+       
+        displayImagem.sprite = listaImagens[indice];
+        campoTitulo.text = titulos[indice];
+        campoResumo.text = resumos[indice];
     }
 }
